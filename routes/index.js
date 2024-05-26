32aed96d59744c0c3ace5218d3ed6e8f39025f77
sarahsteadman <fairyfaye4real@gmail.com>
2024-05-25 21:09:28 -0600

    Swagger Test

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,14 @@ const router = express.Router();
 
 const potionsController = require('../controllers/potions');
 
-router.get('/', potionsController.getAll);
-router.post("/", potionsController.create);
+router.get('/potions', potionsController.getAll);
+router.post("/potions", potionsController.create);
 
-router.get('/:id', potionsController.getSingle);
-router.put('/:id', potionsController.update);
-router.delete('/:id', potionsController.del);
+router.get('/potions:id', potionsController.getSingle);
+router.put('/potions:id', potionsController.update);
+router.delete('/potions:id', potionsController.del);
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
