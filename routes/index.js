c4ed7a9a530e17759281153abe8937fcc7207a29
sarahsteadman <fairyfaye4real@gmail.com>
2024-05-25 21:13:44 -0600

    added swagger requirements

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger_output.json');
 
 const potionsController = require('../controllers/potions');
 
 router.get('/potions', potionsController.getAll);
-router.post("/potions", potionsController.create);
+router.post('/potions', potionsController.create);
 
 router.get('/potions:id', potionsController.getSingle);
 router.put('/potions:id', potionsController.update);
