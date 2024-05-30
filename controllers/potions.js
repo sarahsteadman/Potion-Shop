ba04da7d778a46d74a0166e77d9cdba90f25488b
sarahsteadman <fairyfaye4real@gmail.com>
2024-05-29 21:10:00 -0600

    validations and try catch blocks added

--- controllers/potions.js
+++ controllers/potions.js
@@ -4,83 +4,117 @@ const dotenv = require('dotenv');
 dotenv.config();
 const dbName = process.env.DATABASE_NAME;
 
+const { potionValidationRules } = require('../validation');
+const { validationResult } = require('express-validator');
+
 const getAll = async (req, res, next) => {
-    const potions = await mongodb.getDb().db(dbName).collection('Potions').find();
+    try {
+        const potions = await mongodb.getDb().db(dbName).collection('Potions').find();
 
-    potions.toArray().then((lists) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists);
-    });
-
+        potions.toArray().then((lists) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists);
+        });
+    }
+    catch {
+        res.status(500).json({ error: 'Failed to fetch potions' });
+    }
 };
 
 const getSingle = async (req, res, next) => {
+    try {
+        const idString = String(req.params.id);
+        const potionId = new ObjectId(idString);
+        const result = await mongodb.getDb().db(dbName).collection('Potions').find({ _id: potionId });
 
-    const idString = String(req.params.id);
-    const potionId = new ObjectId(idString);
-    const result = await mongodb.getDb().db(dbName).collection('Potions').find({ _id: potionId });
-
-    result.toArray().then((lists) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists);
-    });
-
+        result.toArray().then((lists) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists);
+        });
+    }
+    catch {
+        res.status(500).json({ error: 'Failed to fetch potion' });
+    }
 };
 
 const create = async (req, res, next) => {
-    const potion = {
-        name: req.body.name,
-        properties: req.body.properties,
-        cost: req.body.cost,
-        price: req.body.price,
-        quantity: req.body.quantity,
-        supplier: req.body.supplier
-    };
-    const response = await mongodb.getDb().db(dbName).collection('Potions').insertOne(potion);
-
-    if (response.acknowledged) {
-        res.status(201).json(response);
-    } else {
-        res.status(500).json(response.error || 'Error! Potion not added.');
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
-    console.log(res.statusCode);
+
+    try {
+        const potion = {
+            name: req.body.name,
+            properties: req.body.properties,
+            cost: req.body.cost,
+            price: req.body.price,
+            quantity: req.body.quantity,
+            supplier: req.body.supplier
+        };
+        const response = await mongodb.getDb().db(dbName).collection('Potions').insertOne(potion);
+
+        if (response.acknowledged) {
+            res.status(201).json(response);
+        } else {
+            res.status(500).json(response.error || 'Error! Potion not added.');
+        }
+        console.log(res.statusCode);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Failed to create potion' });
+    }
 
 };
 
 const update = async (req, res, next) => {
-    const potion = {
-        name: req.body.name,
-        properties: req.body.properties,
-        cost: req.body.cost,
-        price: req.body.price,
-        quantity: req.body.quantity,
-        supplier: req.body.supplier
-    };
-
-    const idString = String(req.params.id);
-    const potionId = new ObjectId(idString);
-    const response = await mongodb.getDb().db(dbName).collection('Potions').replaceOne({ _id: potionId }, potion);
-
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Error! potion not updated.');
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
-    console.log(res.statusCode);
+
+    try {
+        const potion = {
+            name: req.body.name,
+            properties: req.body.properties,
+            cost: req.body.cost,
+            price: req.body.price,
+            quantity: req.body.quantity,
+            supplier: req.body.supplier
+        };
+
+        const idString = String(req.params.id);
+        const potionId = new ObjectId(idString);
+        const response = await mongodb.getDb().db(dbName).collection('Potions').replaceOne({ _id: potionId }, potion);
+
+        if (response.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Error! potion not updated.');
+        }
+        console.log(res.statusCode);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Failed to update potion' });
+    }
 };
 
 const del = async (req, res, next) => {
-    const idString = String(req.params.id);
-    const potionId = new ObjectId(idString);
-    const response = await mongodb.getDb().db(dbName).collection('Potions').deleteOne({ _id: potionId }, true);
-    console.log(response);
-    if (response.deletedCount > 0) {
-        res.status(200).send();
-    } else {
-        res.status(500).json(response.error || 'Error! Unable to delete!');
+    try {
+        const idString = String(req.params.id);
+        const potionId = new ObjectId(idString);
+        const response = await mongodb.getDb().db(dbName).collection('Potions').deleteOne({ _id: potionId }, true);
+        console.log(response);
+        if (response.deletedCount > 0) {
+            res.status(200).send();
+        } else {
+            res.status(500).json(response.error || 'Error! Unable to delete!');
+        }
+        console.log(res.statusCode);
     }
-    console.log(res.statusCode);
-
+    catch (error) {
+        res.status(500).json({ error: 'Failed to delete potion' });
+    }
 };
 
 module.exports = { getAll, getSingle, create, update, del };
---
+++ validation.js
@@ -0,0 +1,36 @@
+const { check } = require('express-validator');
+
+const potionValidationRules = () => {
+    return [
+        body('name')
+            .isString().withMessage('Name must be a string')
+            .notEmpty().withMessage('Name is required')
+            .isLength({ min: 1, max: 100 }).withMessage('Name must be between 1 and 100 characters'),
+
+        body('properties')
+            .isString().withMessage('Properties must be a string')
+            .notEmpty().withMessage('Properties are required')
+            .isLength({ min: 1, max: 255 }).withMessage('Properties must be between 1 and 255 characters'),
+
+        body('cost')
+            .isFloat({ gt: -1 }).withMessage('Cost must be a number greater than or equal to 0')
+            .notEmpty().withMessage('Cost is required'),
+
+        body('price')
+            .isFloat({ gt: -1 }).withMessage('Price must be a number greater than or equal to 0')
+            .notEmpty().withMessage('Price is required'),
+
+        body('quantity')
+            .isInt({ gt: -1 }).withMessage('Quantity must be an integer greater than or equal to 0')
+            .notEmpty().withMessage('Quantity is required'),
+
+        body('supplier')
+            .optional()
+            .isString().withMessage('Supplier must be a string')
+            .isLength({ max: 100 }).withMessage('Supplier must be less than or equal to 100 characters')
+    ];
+};
+
+module.exports = {
+    potionValidationRules
+};
