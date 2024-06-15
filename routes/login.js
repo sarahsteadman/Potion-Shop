d84395a2e8b02d828f68c3c60151233262bd9db8
sarahsteadman <fairyfaye4real@gmail.com>
2024-06-14 20:25:18 -0600

    router fixed

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const loginController = require('../controllers/login');
 
 router.get('/', loginController.githubAuth);
-app.get('/callback', loginController.githubAuthCallback, loginController.githubAuthSuccess);
+router.get('/callback', loginController.githubAuthCallback, loginController.githubAuthSuccess);
 router.get('/profile', loginController.getProfile);
 router.get('/logout', loginController.logout);
 router.get('/loggedOut', loginController.loggedOut);
