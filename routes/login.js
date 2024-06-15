ee1bbc6616439e557140a9141055f815caa40cab
sarahsteadman <fairyfaye4real@gmail.com>
2024-06-14 18:19:20 -0600

    fixed routes

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const loginController = require('../controllers/login');
 
 router.get('/', loginController.githubAuth);
-router.get('/profile', loginController.profile);
+router.get('/profile', loginController.getProfile);
 router.get('/logout', loginController.logout);
 router.get('/loggedOut', loginController.loggedOut);
 
